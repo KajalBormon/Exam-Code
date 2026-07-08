9e32ee4de481a6f8292172e8adbcc7717701b22b
KajalBormon <kajalbormon40@gmail.com>
2026-07-08 23:57:44 +0600

    prime number print

---
+++ ICB Securites & Trading Co./1 to n prime number/prime_number_print.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n, isPrime = 0; 
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    for(int i = 2; i <= n; i++){
+        isPrime = 1; 
+
+        for(int j = 2; j * j <= i; j++){
+            if(i % j == 0){
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if(isPrime){
+            printf("%d ", i);
+        }
+    }
+
+    return 0; 
+}
