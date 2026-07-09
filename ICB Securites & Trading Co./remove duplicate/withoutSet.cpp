f141165dbf4ea260530c66ccbbfcdbc6cbb5c7ee
KajalBormon <kajalbormon40@gmail.com>
2026-07-09 17:25:02 +0600

    Remove duplicate

---
+++ ICB Securites & Trading Co./remove duplicate/withoutSet.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int n; 
+    cout << "Enter the number of elements: ";
+    cin >> n; 
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+
+        bool duplicate = false; 
+
+        for(int j = 0; j < i; j++){
+            if(arr[i] == arr[j]){
+                duplicate = true; 
+                break; 
+            }
+        }
+
+        if(!duplicate){
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0; 
+}
