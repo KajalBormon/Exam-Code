5081ad089c141fcf072820410d94495ea7d76276
KajalBormon <kajalbormon40@gmail.com>
2026-07-09 18:02:26 +0600

    String length

---
+++ ICB Securites & Trading Co./string length/string_length.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    string str; 
+    int count = 0; 
+    cout << "Enter the string: ";
+    getline(cin, str);
+
+    for(int i = 0; str[i] != '\0'; i++){
+        count++; 
+    }
+
+    cout << "String length: " << count << endl; 
+
+    return 0; 
+}
